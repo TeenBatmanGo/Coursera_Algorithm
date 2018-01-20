57a433ed68ba349a952bbe62a7e885f93834d1cb
Wang Chengming <wangchengmingbbs@126.com>
2018-01-20 15:16:50 +0800

    add Topological ordering

--- Graphs/DFS.py
+++ Graphs/DFS.py
@@ -2,20 +2,32 @@
 def dfs_loop(graph):
     v = list(graph.keys())
     n = len(v)
+    path= []
+    # We have to use mutable object like list to store the current number.
+    curr = [n]
+    ordering = dict(zip(v, [n]*n))
     visited = dict(zip(v, [False]*n))
     for vi in v:
         if not visited[vi]:
-            dfs(graph, vi, visited)
+            dfs(graph, vi, visited, path, curr, ordering)
+    return path
 
 
-def dfs(graph, start, visited):
+def dfs(graph, start, visited, path, curr, ordering):
     visited[start] = True
     for w in graph[start]:
         print(start, w, visited[w])
         if not visited[w]:
             visited[w] = True
+            path.append([start, w])
             print('After checking:', start, w)
-            dfs(graph, w, visited)
+            dfs(graph, w, visited, path, curr, ordering)
+    ordering[start] = curr[0]
+    # Use slicing operation
+    curr[0] = curr[0] - 1
+    print('######## Topological Ordering:', ordering)
+
+
 
 
 if __name__ == '__main__':
@@ -32,5 +44,6 @@ if __name__ == '__main__':
                  'C': 'D',
                  'D': ()}
 
-    dfs_loop(testcase2)
+    result = dfs_loop(testcase1)
+    print('\nThe Path:', result)
 
